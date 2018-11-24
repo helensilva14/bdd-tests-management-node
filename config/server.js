143981da2cd451429e5161ad4132402801195a58
Helen Silva <helensilva14@gmail.com>
2018-11-24 20:04:33 +0000

    Added middleware to handle unavailable routes

--- config/server.js
+++ config/server.js
@@ -3,11 +3,6 @@ let consign = require('consign');
 let body_parser = require('body-parser');
 let expressValidator = require('express-validator');
 let expressSession = require('express-session');
-/**
- * This module let us use HTTP verbs such as PUT or DELETE 
- * in places where they are not supported
- */ 
-let methodOverride = require('method-override');
 
 let app = express(); 
 
@@ -26,12 +21,13 @@ app.use(expressSession({
     saveUninitialized: false //cria uma sessão nova toda vez
 }));
  
-/**
- * using custom logic to override method
- * 
- * there are other ways of overriding as well
- * like using header & using query value
+ /**
+ * This module let us use HTTP verbs such as PUT or DELETE 
+ * in places where they are not supported, such as forms
  */ 
+let methodOverride = require('method-override');
+
+// using custom logic to override method
 app.use(methodOverride(function (req, res) {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
     // look in urlencoded POST bodies and delete it
@@ -41,16 +37,15 @@ app.use(methodOverride(function (req, res) {
   }
 }));
 
-// middleware to make 'user' available to all templates
-app.use(function(req, res, next) {
-  res.locals.user = req.session.user;
-  next();
-});
-
 consign().include('app/routes')
 .then('config/dbConnection.js')
 .then('app/models')
 .then('app/controllers')
 .into(app);
+
+// middleware used to handle a 404 (not found) error - unavailable routes
+app.use(function (req, res, next) {
+  res.status(404).send("Essa rota não existe!")
+});
 
 module.exports = app;

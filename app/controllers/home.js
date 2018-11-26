70f25a8785dd6479df00bb54008df5b70cf7d87c
Helen Silva <helen.silva@eldorado.org.br>
2018-11-26 16:07:33 -0200

    Fixed index view. Added missing item on home controller

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,5 @@
 module.exports.index = function (app, req, res) {
+    // make the logged user information available to home template
+    res.locals.user = req.session.user;
     res.render('home/index');
 }

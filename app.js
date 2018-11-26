a143517a21c4604482c0372c3a231f27a4d68c0a
Helen Silva <helen.silva@eldorado.org.br>
2018-11-26 09:38:19 -0200

    Updated server default port

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let app = require('./config/server.js');
 
-app.listen(process.env.PORT || 3000, process.env.IP, function() {
+app.listen(process.env.PORT || 1337, process.env.IP, function() {
 	console.log('Servidor rodando com express');
 });
